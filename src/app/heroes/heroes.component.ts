4c96c8de547d062776dbac15a4afd63d10209283
Tavia Sterling <TaviaSterling@outlook.com>
2018-11-02 10:43:42 -0600

    add hero-detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,15 +8,16 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
   selectedHero: Hero;
   
-  onSelect(hero: Hero): void {
-   this.selectedHero =hero;
-  }
-  heroes = HEROES;
-  constructor() { }
 
+  constructor() { }
+  
   ngOnInit() {
   }
+      onSelect(hero: Hero): void {
+       this.selectedHero =hero;
+      }
 
 }
