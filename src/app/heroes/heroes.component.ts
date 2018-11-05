eece07fb4c96b6190493805513f90f96ef10efb9
Tavia Sterling <TaviaSterling@outlook.com>
2018-11-05 10:16:51 -0700

    routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,21 +9,16 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
   
 
   constructor(private heroService: HeroService) { }
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
-
   ngOnInit() {
     this.getHeroes();
   }
-      onSelect(hero: Hero): void {
-       this.selectedHero =hero;
-      }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
